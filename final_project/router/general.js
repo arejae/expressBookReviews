6868ad2eb63113b15fab28b92591d3af11d28a5e
arejae <arejae@gmail.com>
2024-10-07 17:34:57 +0800

    Task6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,9 +5,18 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 const axios = require('axios');
 
+//  Task 6
+//  Register a new user
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+  if (users.find((user) => user.username === username)) {
+    return res.status(409).json({ message: "Username already exists" });
+  }
+  users.push({ username, password });
+  return res.status(201).json({ message: "User registered successfully" });
 });
 
 // Get book lists
